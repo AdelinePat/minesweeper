846aa92cf1b3743d7f1f5e6d6a348d790704a7e1
Adeline <adeline.patenne@hotmail.fr>
2025-03-25 11:20:59 +0100

    first commit : class diagram

---
+++ __settings__.py
@@ -0,0 +1,23 @@
+from pygame_menu import Theme
+
+## COLORS
+AGRESSIVE_PINK = (195, 60, 84)
+INDIGO_DYE = (37, 78, 112)
+CERULEAN = (55,113, 142)
+CELESTE = (174, 243, 231)
+CYAN = (142, 227, 239)
+GHOST_WHITE = (250, 254, 255)
+NOT_SO_GHOST_WHITE = (212, 223, 228)
+
+CUSTOM_THEME = Theme(
+    background_color=GHOST_WHITE,
+    scrollbar_shadow=True,
+    scrollbar_slider_color="orange",
+    scrollbar_slider_hover_color="green",
+    scrollbar_slider_pad=2,
+    selection_color=AGRESSIVE_PINK,
+    title_background_color=INDIGO_DYE,
+    title_font_color=CELESTE,
+    title_font_shadow=True,
+    widget_font_color=INDIGO_DYE
+)
