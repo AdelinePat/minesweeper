aab0c6d706b6c41b5f00fa44f3c59ee81c4258eb
Adeline <adeline.patenne@hotmail.fr>
2025-03-27 10:35:28 +0100

    feat: game_info in in_game_menu.py and creation of game_board class

--- grid_test copy.py
+++ grid_test copy.py
@@ -7,10 +7,8 @@ import random
 actual_screen = Menu('Grille de jeu')
 pygame.init()
 
-# pygame.display.get_surface().fill((NOT_SO_GHOST_WHITE))  # background
 
 class Board():
-    # alphabet = string.ascii_uppercase
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
@@ -32,288 +30,188 @@ class Board():
                                                self.grid_rect,
                                                border_radius=actual_screen.border_radius)
 
-        # self.grid_border = pygame.draw.rect(actual_screen.screen,
-        #                                  CERULEAN,
-        #                                  self.grid_rect,
-        #                                  actual_screen.border_thickness,
-        #                                  border_radius=actual_screen.border_radius)
         self.padding = 2
 
         self.grid_node_width = (
             (self.grid_rect.height - actual_screen.border_thickness*2 ) // self.rows)- self.padding
         self.grid_node_height = ((self.grid_rect.height - actual_screen.border_thickness*2) // self.rows) - self.padding
-
-        # self.grid_surface_tuple = (actual_screen.width//3*2, actual_screen.height//3*2)
-        
-        # pygame.display.get_surface()
-        # grid_surface = pygame.display.set_mode(grid_surface_tuple)
-        # self.grid_surface = pygame.surface.Surface(self.grid_surface_tuple)
-        # self.grid_fill_rect = self.grid_surface.get_rect()
-        # self.grid_fill_rect.center = actual_screen.screen_center
-        # self.grid_fill = self.grid_surface.fill('black')
-        # self.grid_display = actual_screen.screen.blit(self.grid_surface, self.grid_fill_rect)
-
         self.grid_top_left = (self.grid_rect.topleft[0] + actual_screen.border_thickness+ self.padding//2,
                               self.grid_rect.topleft[1] + actual_screen.border_thickness+ self.padding//2 )
-        # self.grid_top_left = (actual_screen.window_rect.center[0] - actual_screen.window_rect.center[0]//2,
-        #                       actual_screen.window_rect.center[1] - actual_screen.window_rect.center[1] //2)
-
+        
         self.board = self.create_board()
 
     
-    def draw_text(self, text, font, font_size, position, color=INDIGO_DYE):
-        # dialog, dialog_rect = self.create_text_rect()
-        font_load = pygame.font.Font(font, font_size)
-        dialog = font_load.render(text, True, color)
-        dialog_rect = dialog.get_rect(center=position)
-        actual_screen.screen.blit(dialog, dialog_rect)
-        return dialog_rect
-
-        # self.board[5][6] = 1
+    # def draw_text(self, text, font, font_size, position, color=INDIGO_DYE):
+    #     font_load = pygame.font.Font(font, font_size)
+    #     dialog = font_load.render(text, True, color)
+    #     dialog_rect = dialog.get_rect(center=position)
+    #     actual_screen.screen.blit(dialog, dialog_rect)
+    #     return dialog_rect
     
-    def clear_previous_emplacement(self, x, y):
-        self.board[y][x] = None
+    # def createSquare(self,  color, x, y):
+    #     actual_square = pygame.rect.Rect([x, y, self.grid_node_width, self.grid_node_height])
+    #     pygame.draw.rect(actual_screen.screen, color, actual_square)
+
+    #     return actual_square
+    
+
 
     # def create_board(self):
-    #     board = []
-    #     row_list = []
     #     x = self.grid_top_left[0]
     #     y = self.grid_top_left[1]
-
-    #     for index in range(self.rows):
+    #     board = []
+        
+    #     for index in range(self.rows): # rows
     #         row_list = []
+
     #         for other_index in range(self.columns):
-    #             content = None
+    
+    #             square_hitbox = self.createSquare(NOT_SO_GHOST_WHITE, x, y)
+    #             content = [square_hitbox, None]
+
+    #             if (index, other_index) in self.bomb_positions:
+    #                 content = [square_hitbox, 'bomb']
+               
     #             row_list.append(content)
-    #             # self.createSquare(NOT_SO_GHOST_WHITE, x, y)
+    #             x += (self.grid_node_width + self.padding)
+
     #         board.append(row_list)
-    #     return board
-    
-    def createSquare(self,  color, x, y):
-        actual_square = pygame.rect.Rect([x, y, self.grid_node_width, self.grid_node_height])
-        pygame.draw.rect(actual_screen.screen, color, actual_square)
-
-        return actual_square
-    
-    def uupdate_board(self):
-        x = self.grid_top_left[0]
-        y = self.grid_top_left[1]
-        for index in range(self.rows): # rows
-            for other_index in range(self.columns):
-                y += self.grid_node_height
-                if index % 2 == 0 and other_index % 2 == 0:
-                    # self.board[index][other_index] = 1
-                    self.createSquare(GHOST_WHITE, x, y)
-                else:
-                    self.createSquare(NOT_SO_GHOST_WHITE, x, y)
-            
-            x += self.grid_node_width
-            # print(index)
-
-
-    def create_board(self):
-        x = self.grid_top_left[0]
-        y = self.grid_top_left[1]
-        board = []
+    #         x = self.grid_top_left[0]
+    #         y += (self.grid_node_height + self.padding)
         
-        for index in range(self.rows): # rows
-            row_list = []
+    #     self.draw_grid_border()
 
-            for other_index in range(self.columns):
-                # self.createSquare(NOT_SO_GHOST_WHITE, x, y)
-                # if index % 2 == 0:
-                #     if other_index % 2 == 0:
-                square_hitbox = self.createSquare(NOT_SO_GHOST_WHITE, x, y)
-                content = [square_hitbox, None]
-
-                if (index, other_index) in self.bomb_positions:
-                    content = [square_hitbox, 'bomb']
-                #     else:
-                #         square_hitbox = self.createSquare(GHOST_WHITE, x, y)
-                #         content = [square_hitbox, None]
-                # else:
-                #     if other_index % 2 != 0:
-                #         square_hitbox = self.createSquare(NOT_SO_GHOST_WHITE, x, y)
-                #         content = [square_hitbox, None]
-                #     else:
-                #         square_hitbox = self.createSquare(GHOST_WHITE, x, y)
-                #         content = [square_hitbox, None]
-                
-                row_list.append(content)
-                x += (self.grid_node_width + self.padding)
-
-            board.append(row_list)
-            x = self.grid_top_left[0]
-            y += (self.grid_node_height + self.padding)
-        
-        self.draw_grid_border()
-
-            # print(board)
-        return board
+        # return board
     
-    def set_range(self, value, max_value):
-        if value == 0:
-            min_range = 0
-            max_range = value + 2
-        elif value == (max_value-1):
-            min_range = value - 1
-            max_range = value 
-        else:
-            min_range = value - 1
-            max_range = value + 2
+    # def set_range(self, value, max_value):
+    #     if value == 0:
+    #         min_range = 0
+    #         max_range = value + 2
+    #     elif value == (max_value-1):
+    #         min_range = value - 1
+    #         max_range = value + 1
+    #     else:
+    #         min_range = value - 1
+    #         max_range = value + 2
         
-        return min_range, max_range
+    #     return min_range, max_range
 
-    
-
-    def count_surrounding_bombs(self, row, column):
-        bomb_count = 0
-        min_row_range, max_row_range = self.set_range(row, self.rows)
-        min_column_range, max_column_range = self.set_range(column, self.columns)
+    # def count_surrounding_bombs(self, row, column):
+    #     bomb_count = 0
+    #     min_row_range, max_row_range = self.set_range(row, self.rows)
+    #     min_column_range, max_column_range = self.set_range(column, self.columns)
        
-        for actual_row in range(min_row_range, max_row_range):
-            for actual_col in range(min_column_range, max_column_range):
-                if (actual_row, actual_col) in self.bomb_positions:     
-                    bomb_count += 1
+    #     for actual_row in range(min_row_range, max_row_range):
+    #         for actual_col in range(min_column_range, max_column_range):
+    #             if (actual_row, actual_col) in self.bomb_positions:     
+    #                 bomb_count += 1
 
-            # self.board[row][column][1] = bomb_count
-
-        # print(self.board[row][column][1])
-        return bomb_count
+    #     return bomb_count
         
-    def set_board_values(self):
-        for bomb_position in self.bomb_positions:
-            x = bomb_position[0]
-            y = bomb_position[1]
-            self.board[x][y][1] = 'bomb'
+    # def set_board_values(self):
+    #     for bomb_position in self.bomb_positions:
+    #         x = bomb_position[0]
+    #         y = bomb_position[1]
+    #         self.board[x][y][1] = 'bomb'
 
-        for row in range(self.rows):
-            for column in range(self.columns):
-                if self.board[row][column][1] == None:
-                    self.board[row][column][1] = self.count_surrounding_bombs(row, column)
-                print(f'value at {row},{column} : {self.board[row][column][1]}')
+    #     for row in range(self.rows):
+    #         for column in range(self.columns):
+    #             if self.board[row][column][1] == None:
+    #                 self.board[row][column][1] = self.count_surrounding_bombs(row, column)
+    #             print(f'value at {row},{column} : {self.board[row][column][1]}')
 
     
-    def check_user_click(self,row, column, mouse_position, hitbox):
-        if hitbox[0].collidepoint(mouse_position) and pygame.mouse.get_pressed()[0]:
-            self.click += 1
-            if self.click == 1:
-                self.bomb_positions = self.get_bomb_positions_list(row, column)
-                self.set_board_values()
+    # def check_user_click(self,row, column, mouse_position, hitbox):
+    #     if hitbox[0].collidepoint(mouse_position) and pygame.mouse.get_pressed()[0]:
+    #         self.click += 1
+    #         if self.click == 1:
+    #             self.bomb_positions = self.get_bomb_positions_list(row, column)
+    #             self.set_board_values()
 
-            if (row, column) in self.bomb_positions:
-                print("AIE, raté ! Il y avait une bombe , tu as perdu")
-            else:
-                self.reveal_square(row, column)
-                # self.count_surrounding_bombs(row, column)
-                # hitbox[1] = 2
-                # hitbox[0] = self.createSquare(GHOST_WHITE,
-                #             hitbox[0].topleft[0],
-                #             hitbox[0].topleft[1])
-                # if hitbox[1] in (1,2,3,4,5,6,7,8):
-                #     self.draw_text(str(hitbox[1]),
-                #             TEXT_FONT,
-                #             hitbox[0].height - 5,
-                #             hitbox[0].center
-                #             )
-                
-                
-                # print(hitbox[0].topleft)
-                # print(f'value of square = {hitbox[1]}')
-    def draw_grid_border(self):
-        self.grid_border = pygame.draw.rect(actual_screen.screen,
-                                         CERULEAN,
-                                         self.grid_rect,
-                                         actual_screen.border_thickness,
-                                         border_radius=actual_screen.border_radius)
+    #         if (row, column) in self.bomb_positions:
+    #             print("AIE, raté ! Il y avait une bombe , tu as perdu")
+    #         else:
+    #             self.reveal_square(row, column)
+               
+    # def draw_grid_border(self):
+    #     self.grid_border = pygame.draw.rect(actual_screen.screen,
+    #                                      CERULEAN,
+    #                                      self.grid_rect,
+    #                                      actual_screen.border_thickness,
+    #                                      border_radius=actual_screen.border_radius)
 
-    def reveal_square(self, row, column):
-        min_row_range, max_row_range = self.set_range(row, self.rows)
-        min_column_range, max_column_range = self.set_range(column, self.columns)
+    # def reveal_square(self, row, column):
+    #     min_row_range, max_row_range = self.set_range(row, self.rows)
+    #     min_column_range, max_column_range = self.set_range(column, self.columns)
 
         
-        for actual_row in range(min_row_range, max_row_range):
-            for actual_col in range(min_column_range, max_column_range):
+    #     for actual_row in range(min_row_range, max_row_range):
+    #         for actual_col in range(min_column_range, max_column_range):
                 
-                if self.board[actual_row][actual_col][1] == 0 and (actual_row,actual_col) not in self.revealed_square:
+    #             if self.board[actual_row][actual_col][1] == 0 and (actual_row,actual_col) not in self.revealed_square:
 
-                    self.createSquare(GHOST_WHITE,
-                        self.board[actual_row][actual_col][0].topleft[0],
-                        self.board[actual_row][actual_col][0].topleft[1])
+    #                 self.createSquare(GHOST_WHITE,
+    #                     self.board[actual_row][actual_col][0].topleft[0],
+    #                     self.board[actual_row][actual_col][0].topleft[1])
                     
-                    # font, font_size, position
-                    # self.draw_text(str(self.board[actual_row][actual_col][1]),
-                    #                TEXT_FONT,
-                    #                self.board[actual_row][actual_col][0].height - 5,
-                    #                self.board[actual_row][actual_col][0].center
-                    #                )
-            
-                    position = (actual_row, actual_col)
-                    self.revealed_square.append(position)
-                    print(position)
-                    self.reveal_square(actual_row, actual_col)
+    #                 position = (actual_row, actual_col)
+    #                 self.revealed_square.append(position)
+    #                 print(position)
+    #                 self.reveal_square(actual_row, actual_col)
 
-                elif self.board[actual_row][actual_col][1] in (1,2,3,4,5,6,7,8):
-                    self.createSquare(GHOST_WHITE,
-                        self.board[actual_row][actual_col][0].topleft[0],
-                        self.board[actual_row][actual_col][0].topleft[1])
+    #             elif self.board[actual_row][actual_col][1] in (1,2,3,4,5,6,7,8):
+    #                 self.createSquare(GHOST_WHITE,
+    #                     self.board[actual_row][actual_col][0].topleft[0],
+    #                     self.board[actual_row][actual_col][0].topleft[1])
 
-                    self.draw_text(str(self.board[actual_row][actual_col][1]),
-                        TEXT_FONT,
-                        self.board[actual_row][actual_col][0].height - 5,
-                        self.board[actual_row][actual_col][0].center
-                        )
+    #                 self.draw_text(str(self.board[actual_row][actual_col][1]),
+    #                     TEXT_FONT,
+    #                     self.board[actual_row][actual_col][0].height - 5,
+    #                     self.board[actual_row][actual_col][0].center
+    #                     )
                     
-            self.draw_grid_border()
-                
-        print(self.revealed_square)
+    #         self.draw_grid_border()
 
-
-    def go_through_board(self):
-        mouse_position = pygame.mouse.get_pos()
-        for row in range(self.rows):
-            for column in range(self.columns):
-                hitbox = self.board[row][column]
-                self.check_user_click(row, column, mouse_position, hitbox)
+    # def go_through_board(self):
+    #     mouse_position = pygame.mouse.get_pos()
+    #     for row in range(self.rows):
+    #         for column in range(self.columns):
+    #             hitbox = self.board[row][column]
+    #             self.check_user_click(row, column, mouse_position, hitbox)
     
-    def get_random_position_tuple(self):
-        x = random.randrange(self.rows)
-        y = random.randrange(self.columns)
-        position = (x,y)
-        return position
+    # def get_random_position_tuple(self):
+    #     x = random.randrange(self.rows)
+    #     y = random.randrange(self.columns)
+    #     position = (x,y)
+    #     return position
 
-    def get_bomb_positions_list(self, row, column):
+    # def get_bomb_positions_list(self, row, column):
 
-        bomb_positions_list = []
+    #     bomb_positions_list = []
 
-        bomb_number = random.randrange(5,10)
+    #     bomb_number = random.randrange(5,10)
 
-        for bomb in range(0, bomb_number):
-            position = self.get_random_position_tuple()
-            while position in bomb_positions_list or position == (row, column):
-                position = self.get_random_position_tuple()
-            bomb_positions_list.append(position)
-        print(bomb_positions_list)
-        return bomb_positions_list
+    #     for bomb in range(0, bomb_number):
+    #         position = self.get_random_position_tuple()
+    #         while position in bomb_positions_list or position == (row, column):
+    #             position = self.get_random_position_tuple()
+    #         bomb_positions_list.append(position)
+    #     print(bomb_positions_list)
+    #     return bomb_positions_list
                 
 
     
         
 
-# we use the sizes to draw as well as to do our "steps" in the loops. 
 matrix = Board(10,10)
-# matrix_grid = matrix.create_board()
-
 
 board = matrix.create_board()
-# visualizeGrid()  # call the function    
 while True:
     
 
     actual_screen.update()
     
     matrix.go_through_board()
-    # matrix.visualizeGrid()  # call the function    
     for event in pygame.event.get():
         
         if event.type == pygame.QUIT:
@@ -323,7 +221,3 @@ while True:
         if event.type == pygame.KEYDOWN:
         
             print("A key has been pressed")
-
-    
-    
-      # keeps the window open so you can see the result.
--- view/in_game_menu.py
+++ view/in_game_menu.py
@@ -1,4 +1,44 @@
-from view.menu import Menu
+# from view.menu import Menu
+from view.interface import Interface
+from view.__settings__ import CELESTE, TITLE_FONT, TEXT_FONT, NOT_SO_GHOST_WHITE, CYAN, GHOST_WHITE, INDIGO_DYE
+import random
+# from model.game_board import GameBoard
+import pygame
 
-class InGameMenu(Menu):
-    pass
+pygame.init()
+class InGameMenu(Interface):
+    def __init__(self, caption):
+        super().__init__(caption)
+    
+    def set_title(self, text):
+        self.title = self.draw_text(text,
+            TITLE_FONT,
+            self.height//9,
+            (self.title_center[0] // 4*3, self.title_center[1]),
+            color=CELESTE
+        )
+
+    def display_game_info(self, text_title, text_content, position_y):
+        font = TEXT_FONT
+        font_size = self.height // 24
+        color = CYAN
+        color_content = GHOST_WHITE
+        position = (self.width // 4*3, position_y)
+        position_content = (self.width // 4*3, position_y + font_size*1.5)
+        
+
+        content_rect = self.draw_text(text_content, font, font_size*2, position_content, color_content)
+
+        back_rect = pygame.Rect(0, 0,
+                    content_rect.width + 100,
+                    content_rect.height)
+        
+        back_rect.center = content_rect.center
+        
+        # pygame.draw.rect(content_rect)
+        # self.screen, self.win_bg_color, self.window_rect
+        pygame.draw.rect(self.screen, INDIGO_DYE, back_rect)
+        content_rect = self.draw_text(text_content, font, font_size*2, position_content, color_content)
+        title_rect = self.draw_text(text_title, TITLE_FONT, font_size, position, color)
+        
+        return title_rect
